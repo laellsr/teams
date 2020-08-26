fe7b1c0e62e9ba49fb4968994245cbc9b3fb6c6c
laellsr <lael_lsr@hotmail.com>
2020-08-26 16:07:32 -0300

    att

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header();?>
+
+        <!-- about begin -->
+<h2>404</h2>
+<h5>Sorry!</h5>
+        <!-- about end -->
+        
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,213 @@
+        <!-- footer begin -->
+        <div class="footer" id="contact">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-xl-4 col-lg-5 col-md-10">
+                        <div class="about-widget">
+                            <a class="logo" href="index.html">
+                                <img src="<?php bloginfo('template_url'); ?>/assets/img/logo.png" alt="">
+                            </a>
+                            <p>PerediOn offers you all the best online prediction from every corner of the planet with thousands of online prediction markets.</p>
+                            <div class="social">
+                                <ul>
+                                    <li>
+                                        <a href="#" class="social-icon">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                        <a href="#" class="social-icon">
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                        <a href="#" class="social-icon">
+                                            <i class="fab fa-instagram"></i>
+                                        </a>
+                                        <a href="#" class="social-icon">
+                                            <i class="fab fa-pinterest-p"></i>
+                                        </a>
+                                        <a href="#" class="social-icon">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="support">
+                                <ul>
+                                    <li>
+                                        <span class="icon">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/svg/email.svg" alt="">
+                                        </span>
+                                        <span class="text">
+                                            <span class="title">Mail Us</span>
+                                            <span class="descr">don't@reply.com</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <span class="icon">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/svg/phone-call.svg" alt="">
+                                        </span>
+                                        <span class="text">
+                                            <span class="title">Get In Touch</span>
+                                            <span class="descr">+88 015 00 00 00</span>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-3">
+                        <div class="useful-links">
+                            <h3>Useful links</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">IN-PLAY</a>
+                                </li>
+                                <li>
+                                    <a href="#">PROMOTIONS</a>
+                                </li>
+                                <li>
+                                    <a href="#">STATICS</a>
+                                </li>
+                                <li>
+                                    <a href="#">RESULTS</a>
+                                </li>
+                                <li>
+                                    <a href="#">Predict now</a>
+                                </li>
+                                <li>
+                                    <a href="#">GAME RESULTS</a>
+                                </li>
+                                <li>
+                                    <a href="#">STANDINGS</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-md-3">
+                        <div class="useful-links">
+                            <h3>Connect With Us</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="#">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="#">Blog Details</a>
+                                </li>
+                                <li>
+                                    <a href="#">Service</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact us</a>
+                                </li>
+                                <li>
+                                    <a href="#">TEAM OVERVIEW</a>
+                                </li>
+                                <li>
+                                    <a href="#">SCHEDULE</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-2 col-md-3">
+                        <div class="useful-links">
+                            <h3>All Sports</h3>
+                            <ul>
+                                <li>
+                                    <a href="#">FOOTBALL</a>
+                                </li>
+                                <li>
+                                    <a href="#">TENNIS</a>
+                                </li>
+                                <li>
+                                    <a href="#">BASKETBALL</a>
+                                </li>
+                                <li>
+                                    <a href="#">ICE HOCKEY</a>
+                                </li>
+                                <li>
+                                    <a href="#">VOLLEYBALL</a>
+                                </li>
+                                <li>
+                                    <a href="#">BADMINTON</a>
+                                </li>
+                                <li>
+                                    <a href="#">BASEBALL</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-12 col-lg-12">
+                                <div class="payment-method">
+                                    <h6 class="payment-method-title">
+                                        Payment methods are We accept 
+                                    </h6>
+                                    <div class="all-method">
+                                        <div class="single-method">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/brand-1.png" alt="">
+                                        </div>
+                                        <div class="single-method">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/brand-2.png" alt="">
+                                        </div>
+                                        <div class="single-method">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/brand-3.png" alt="">
+                                        </div>
+                                        <div class="single-method">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/brand-4.png" alt="">
+                                        </div>
+                                        <div class="single-method">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/brand-5.png" alt="">
+                                        </div>
+                                        
+                                        <div class="single-method">
+                                            <img src="<?php bloginfo('template_url'); ?>/assets/img/brand-5.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer end -->
+
+        <!-- notes begin -->
+        <div class="notes">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-10 col-lg-10">
+                        PerediOn is a sports prediction html template, published by autworks throgh envato marketplace. You can use this template on any kind of sports prediction in the world. as like football, baseball, soccer, cricket etc. all rights reserved by autworks © 2020.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- notes end -->
+
+        <!-- copyright footer begin -->
+        <div class="copyright-footer">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-xl-5 col-md-6 col-lg-6 d-lg-flex d-lg-flex d-block align-items-center">
+                        <p class="copyright-text">
+                            <a href="#">PerediOn</a> © 2020. PRIVACY POLICY
+                        </p>
+                    </div>
+                    <div class="text-right col-md-6 col-xl-4 col-lg-6 d-xl-flex d-lg-flex d-block align-items-center">
+                        <p class="copyright-text">
+                            Powerd By <a href="https://themeforest.net/user/autworks/portfolio">Autworks ( Envato Author )</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- copyright footer end -->
+</body>
+<?php wp_footer(); ?>
+</html>
+        
+        
