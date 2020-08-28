3d7c316d7ae702469f6d78a1e0376a34d5e13e7d
laellsr <lael_lsr@hotmail.com>
2020-08-28 17:33:16 -0300

    Update page-teams.php

--- page-teams.php
+++ page-teams.php
@@ -2,35 +2,34 @@
 get_header(); ?>
 
 <div class="standing">
-    
 
-                    <?php query_posts('post_type=Teams&post_per_page=-1'); ?>
-                          
-                    <?php if(have_posts()):
+    <?php query_posts('post_type=Teams&post_per_page=-1'); ?>
+          
+    <?php if(have_posts()):
 
-                        while(have_posts()): the_post(); ?>
-                            <div class="container">
-        <div class="standing-list-cover">
-            <div class="standing-team-list">
-                            <div class="col-8 mt-3">
-                                <h4 class="result-title"><?php the_title(); ?></h4>
-                                <div class="mt-1 mb-2"><?php the_field('descricao'); ?> </div>
-                                <dt class="d-inline">Ranked Points</dt>: <span class="diff"><?php the_field('ranked_points'); ?></span>
-                                <span class="logo"><img src="<?php the_field('imagem'); ?>"></span>
-                            </div>
-
+        while(have_posts()): the_post(); ?>
+            <div class="container">
+                <div class="standing-list-cover col-3">
+                    <div class="standing-team-list">
+                        <div class="mb-3">
+                            <span class="logo"><img class="mb-4"> src="<?php the_field('imagem'); ?>"></span>
+                            <h4 class="result-title"><?php the_title(); ?></h4>
+                            <div class="mt-1 mb-2"><?php the_field('descricao'); ?> </div>
+                            <dt class="d-inline">Ranked Points</dt>: <span class="diff"><?php the_field('ranked_points'); ?></span>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 
-                    <?php endwhile; ?>
+    <?php endwhile; ?>
 
-                    <?php else: ?> No team.
+    <?php else: ?> No team.
 
-                    <?php wp_reset_query(); ?>
+    <?php wp_reset_query(); ?>
 
-                    <?php endif; ?>
-            
+    <?php endif; ?>
+
 
 </div>
 
