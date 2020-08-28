b17b0c066f49b460431a73d6735a605aedfcd94f
laellsr <lael_lsr@hotmail.com>
2020-08-28 20:38:28 -0300

    Create post.php

---
+++ post.php
@@ -0,0 +1,35 @@
+<?php get_header();?>
+
+
+                                <?php  query_posts('post_type=Teams&meta_key=ranked_points&orderby=meta_value&order=DESC&post_per_page=-1'); $counter=1; ?>
+                                      
+                                <?php if(have_posts()):
+
+                                    while(have_posts()): the_post(); ?>
+                             
+                                   <tr>
+                                        <th scope="row"><?php echo $counter++; ?></th>
+                                        <td>
+                                            <span class="single-team">
+                                                <span class="logo">
+                                                    <img src="<?php the_field('imagem'); ?>" alt="">
+                                                </span>
+                                                <span class="text">
+                                                    <?php the_title(); ?>
+                                                </span>
+                                            </span>
+                                        </td>
+                                        <td><?php the_field('ranked_points'); ?></td>
+                                    </tr>
+                                    
+                                <?php endwhile; ?>
+
+                                <?php else: ?> No team.
+
+                                <?php wp_reset_query(); ?>
+
+                                <?php endif; ?>
+
+        <!-- standing end -->
+        
+<?php get_footer(); ?>
