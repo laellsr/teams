0b3c9150383b8c79baafd7aa57e0744690ada8f8
laellsr <lael_lsr@hotmail.com>
2020-08-28 17:23:26 -0300

    Update page-teams.php

--- page-teams.php
+++ page-teams.php
@@ -2,21 +2,26 @@
 get_header(); ?>
 
 <div class="standing">
-    <div class="container">
-        <div class="standing-list-cover">
-            <div class="standing-team-list">
-                <div class="row">
+    
+
                     <?php query_posts('post_type=Teams&post_per_page=-1'); ?>
                           
                     <?php if(have_posts()):
 
                         while(have_posts()): the_post(); ?>
+                            <div class="container">
+        <div class="standing-list-cover">
+            <div class="standing-team-list">
                             <div class="col-8 mt-3">
                                 <h4 class="result-title"><?php the_title(); ?></h4>
                                 <div class="mt-1 mb-2"><?php the_field('descricao'); ?> </div>
                                 <dt class="d-inline">Ranked Points</dt>: <span class="diff"><?php the_field('ranked_points'); ?></span>
                                 <span class="logo"><?php the_field('imagem'); ?></span>
                             </div>
+                              
+            </div>
+        </div>
+    </div>
 
                     <?php endwhile; ?>
 
@@ -26,10 +31,7 @@ get_header(); ?>
 
                     <?php endif; ?>
             
-                </div>
-            </div>
-        </div>
-    </div>
+
 </div>
 
 <?php
