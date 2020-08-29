f3bf26fe74b89f54ece4321a501ad53e695ee3b4
laellsr <lael_lsr@hotmail.com>
2020-08-28 22:22:51 -0300

    Update single-teams.php

--- single-teams.php
+++ single-teams.php
@@ -7,7 +7,7 @@ get_header(); ?>
         <div class="row">
             <div class="col-xl-7 col-lg-7">
                 <div class="breadcrumb-content">
-                    <h2>Teams</h2>
+                    <h2><?php the_title(); ?></h2>
                     <ul>
                         <li>
                             Home
@@ -34,37 +34,21 @@ get_header(); ?>
     }
 </style>
 <div class="standing text-center">
-    <div class="row ml-5 mr-5">
-
-        <?php  query_posts('post_type=Teams&order=ASC&post_per_page=-1'); ?>
-              
-        <?php if(have_posts()):
-
-            while(have_posts()): the_post(); ?>
-                    <div class="standing-list-cover col-3">
-                        <div class="standing-team-list">
-                            <div class="mb-3">
-                                <div class="">
-                                    <span class="logo"><a target="_blank" href="<?php the_field('imagem'); ?>"><img class="" src="<?php the_field('imagem'); ?>" style="width: 110px; height: 50%"></a></span>
-                                </div>
-                                <div class="mt-4">
-                                    <a href="<?php the_permalink(); ?>"><h4 class="result-title"><?php the_title(); ?></h4></a>
-                                    <!--<div class="mt-1 mb-2"><?php // the_field('descricao'); ?> </div>-->
-                                    <dt class="d-inline">Ranked Points</dt>: <span class="diff"><?php the_field('ranked_points'); ?></span>
-                                </div>
-                            </div>
-                        </div>
+    <div class="container ml-5 mr-5">
+        <div class="standing-list-cover">
+            <div class="standing-team-list">
+                <div class="mb-3">
+                    <div class="">
+                        <span class="logo"><a target="_blank" href="<?php the_field('imagem'); ?>"><img class="" src="<?php the_field('imagem'); ?>" style="width: 110px; height: 50%"></a></span>
                     </div>
-
-        <?php endwhile; ?>
-
-        <?php else: ?> No team.
-
-        <?php wp_reset_query(); ?>
-
-        <?php endif; ?>
-
-
+                    <div class="mt-4">
+                        <h4 class="result-title"><?php the_title(); ?></h4>
+                        <!--<div class="mt-1 mb-2"><?php // the_field('descricao'); ?> </div>-->
+                        <dt class="d-inline">Ranked Points</dt>: <span class="diff"><?php the_field('ranked_points'); ?></span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
