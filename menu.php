29020c52a709bd746f30108ad4bcacfa9459b6e9
laellsr <lael_lsr@hotmail.com>
2020-08-28 23:08:24 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,8 +2,17 @@
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
     	<ul class="navbar-nav ml-auto">
 				<?php
-					wp_nav_menu(array('theme_location' => 'nav-menu'));
+					//wp_nav_menu(array('theme_location' => 'nav-menu'));
 				?>
+            <li class="nav-item">
+                <a class="nav-link" href="http://laelteste-com.umbler.net/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://laelteste-com.umbler.net/teams/">Teams</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://laelteste-com.umbler.net/standing/">Standing</a>
+            </li>
 		</ul>
 	</div>		
 </nav>
